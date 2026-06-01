fbc8aad14311d178c6fb05cb48275c44e36c2f46
William1738777 <sparringbible@gmail.com>
2026-06-02 07:43:17 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,14 @@
 // --- ASSET MANAGEMENT ---
 const images = {
-    grizzly: new Image(), pyro: new Image(), scorpion: new Image(), dreadnaught: new Image(), seraph: new Image(), destroyer: new Image(),
+    grizzly: new Image(), pyro: new Image(), scorpion: new Image(), dreadnaught: new Image(), seraph: new Image(), destroyer: new Image(), phantomA: new Image(), phantomB: new Image(),
     missile: new Image(), cluster: new Image(), arrow: new Image(), bg1: new Image(), bg2: new Image(), goo: new Image(), lightning: new Image(), static: new Image(),
-    destroRocket: new Image(), destroMissile: new Image(), target: new Image(), firebolt: new Image()
+    destroRocket: new Image(), destroMissile: new Image(), target: new Image(), firebolt: new Image(), phantomMissile: new Image(), phantomSGMissile: new Image()
 };
 
 let loadedCount = 0;
 function onAssetLoad() {
     loadedCount++;
-    if (loadedCount === 18) { // Increased to 18 for Firebolt
+    if (loadedCount === 22) { // Increased to 22 for the 4 new Phantom assets
         const btnStart = document.getElementById('btn-start');
         if (btnStart) {
             btnStart.innerText = "Single Player"; 
@@ -41,6 +41,12 @@ images.destroMissile.onload = onAssetLoad; images.destroMissile.src = 'assets/De
 images.target.onload = onAssetLoad; images.target.src = 'assets/Target.png';
 images.firebolt.onload = onAssetLoad; images.firebolt.src = 'assets/Firebolt.png';
 
+// Phantom Assets
+images.phantomA.onload = onAssetLoad; images.phantomA.src = 'assets/PhantomFormA.png';
+images.phantomB.onload = onAssetLoad; images.phantomB.src = 'assets/PhantomFormB.png';
+images.phantomMissile.onload = onAssetLoad; images.phantomMissile.src = 'assets/PhantomMissile.png';
+images.phantomSGMissile.onload = onAssetLoad; images.phantomSGMissile.src = 'assets/PhantomSGMissile.png';
+
 // --- GAME CONFIG & DATA ---
 const tanksData = [
     { id: 'grizzly', name: 'GRIZZLY', img: images.grizzly, color: '#b533ff', desc: 'Normal [C | \']: Cannon (10)<br>Skill 1 [X | ;]: Cluster (6)<br>Skill 2 [Z | L]: Heavy Missile (15)' },
@@ -48,7 +54,8 @@ const tanksData = [
     { id: 'scorpion', name: 'SCORPION', img: images.scorpion, color: '#00ff66', desc: 'Normal [C | \']: Toxic Burst (3x3)<br>Skill 1 [X | ;]: Hook Arrow (Pull & Jam)<br>Skill 2 [Z | L]: Poison Leak (DoT)' },
     { id: 'dreadnaught', name: 'DREADNAUGHT', img: images.dreadnaught, color: '#8b5a2b', desc: 'Normal [C | \']: Heavy Shell (14)<br>Skill 1 [X | ;]: Dual MG (0.6/shot)<br>Skill 2 [Z | L]: Field Mine (20 Dmg/Knockback)', maxHp: 150, speedMod: 0.5, scaleMod: 1.2 },
     { id: 'seraph', name: 'SERAPH', img: images.seraph, color: '#00ffff', desc: 'Normal [C | \']: Shock Bullet (5 Dmg, 5th shot AoE)<br>Skill 1 [X | ;]: Spark Missile (Emitter)<br>Skill 2 [Z | L]: Lightning Beam (Hold, requires Energy)', maxHp: 100 },
-    { id: 'destroyer', name: 'DESTROYER', img: images.destroyer, color: '#888888', desc: 'Normal [C | \']: Cannon (10)<br>Skill 1 [X | ;]: Mortar Strike (Hold to Lock)<br>Skill 2 [Z | L]: Ghost Missile (Pierces Walls, Wall-Slam Stun)', speedMod: 0.85, maxHp: 120 }
+    { id: 'destroyer', name: 'DESTROYER', img: images.destroyer, color: '#888888', desc: 'Normal [C | \']: Cannon (10)<br>Skill 1 [X | ;]: Mortar Strike (Hold to Lock)<br>Skill 2 [Z | L]: Ghost Missile (Pierces Walls, Wall-Slam Stun)', speedMod: 0.85, maxHp: 120 },
+    { id: 'phantom', name: 'PHANTOM', img: images.phantomA, color: '#9d00ff', desc: 'Normal [C | \']: Dual Mode Missiles (Hold C to Swap)<br>Skill 1 [X | ;]: Plasma Shotgun<br>Skill 2 [Z | L]: Evasive Maneuvers' }
 ];
 
 // --- AUDIO ASSETS ---
