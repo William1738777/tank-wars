b1a95bc5fe4d0cd0c458f3369607df27c29e2746
William1738777 <sparringbible@gmail.com>
2026-06-01 09:10:40 +0800

    Update Projectile.js

--- js/classes/Projectile.js
+++ js/classes/Projectile.js
@@ -5,14 +5,31 @@ class Projectile {
         this.speed = speed; this.radius = radius; this.damage = damage; 
         this.color = color; this.type = type; this.bounces = bounces;
         this.life = type === 'missile' ? 70 : (type === 'arrow' ? 45 : 999); 
-        this.dead = false;
-        this.isFifth = false; 
-
-        // Non-MG bullets have 3 HP to withstand exactly 3 MG bullets
+        this.dead = false; this.isFifth = false; 
         this.projectileHp = type === 'mg' ? 1 : 3;
     }
 
     update() {
+        if (this.type === 'destro_missile') {
+            createParticles(this.x, this.y, 2, '#ff4500', 2, 0.3);
+            this.x += this.vx; this.y += this.vy;
+            if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) this.dead = true;
+            return; // Passes through all walls!
+        }
+        
+        if (this.type === 'destro_rocket') {
+            this.x += this.vx; this.y += this.vy;
+            if (this.y >= this.targetY) { this.y = this.targetY; this.triggerExplosion(); }
+            return;
+        }
+
+        if (this.type === 'destro_up') {
+            this.x += this.vx; this.y += this.vy; this.life--;
+            if (this.life <= 0) this.dead = true;
+            createParticles(this.x, this.y + 10, 1, '#ffaa00', 1, 0.2);
+            return;
+        }
+
         if (this.type === 'swarm') {
             this.timer++;
             if (this.timer % 6 === 0) this.targetAngle = this.baseAngle + (Math.random() - 0.5) * 1.5;
@@ -25,9 +42,7 @@ class Projectile {
 
         if (this.type === 'seraph_c' || this.type === 'seraph_x' || this.type === 'seraph_spark') {
             createParticles(this.x + (Math.random()-0.5)*15, this.y + (Math.random()-0.5)*15, 1, '#00ffff', 1.5, 0.3);
-            if (Math.random() > 0.5) {
-                createParticles(this.x, this.y, 1, '#fff', 2, 0.2);
-            }
+            if (Math.random() > 0.5) createParticles(this.x, this.y, 1, '#fff', 2, 0.2);
         } else if (this.type === 'toxic_bullet') {
             createParticles(this.x, this.y, 1, 'rgba(0, 255, 102, 0.5)', 2, 0.2); 
         } else if (this.type === 'dread_c') {
@@ -94,14 +109,27 @@ class Projectile {
             createParticles(this.x, this.y, 8, '#00ff66', 1.5, 0.5); 
         } else if (this.type === 'seraph_c') {
             createParticles(this.x, this.y, 8, '#00ffff', 1.5, 0.5);
-            if (this.isFifth) {
-                hazards.push({ owner: this.owner, type: 'seraph_aoe', x: this.x, y: this.y, radius: 140, life: 360 });
-            }
+            if (this.isFifth) hazards.push({ owner: this.owner, type: 'seraph_aoe', x: this.x, y: this.y, radius: 140, life: 360 });
         } else if (this.type === 'seraph_x') {
             createParticles(this.x, this.y, 15, '#00ffff', 2.0, 0.5);
             hazards.push({ owner: this.owner, type: 'seraph_emitter', x: this.x, y: this.y, radius: 10, life: 240 });
-        } else if (this.type === 'seraph_spark') {
-            createParticles(this.x, this.y, 5, '#00ffff', 1, 0.4);
+        } else if (this.type === 'destro_missile') {
+            createKaboom(this.x, this.y, 2.5);
+        } else if (this.type === 'destro_rocket') {
+            createKaboom(this.x, this.y, 1.5);
+            // AoE Blast for Mortar
+            players.forEach(tank => {
+                if (tank.owner !== this.owner && !tank.isDead && tank.invulnTimer <= 0) {
+                    if (Math.hypot(tank.x - this.x, tank.y - this.y) < tank.radius + 60) {
+                        tank.hp -= 12;
+                        tank.stunTimer = Math.max(tank.stunTimer, 15);
+                        let angle = Math.atan2(tank.y - this.y, tank.x - this.x);
+                        tank.kbX = Math.cos(angle) * 8; tank.kbY = Math.sin(angle) * 8; tank.kbTimer = 10;
+                        tank.destroSlowTimer = Math.max(tank.destroSlowTimer || 0, 120);
+                        floatingTexts.push({x: tank.x, y: tank.y - 40, text: "BOMBED!", life: 50, color: '#ff4500'});
+                    }
+                }
+            });
         } else {
             createKaboom(this.x, this.y, this.type === 'missile' ? 1.5 : 1.0);
         }
@@ -128,28 +156,24 @@ class Projectile {
         } else if (this.type === 'dread_c') {
             ctx.beginPath(); ctx.arc(0, 0, this.radius, 0, Math.PI*2);
             ctx.fillStyle = '#ff6600'; ctx.shadowBlur = 15; ctx.shadowColor = '#ff3300'; ctx.fill();
-            ctx.beginPath(); ctx.arc(0, 0, this.radius * 0.6, 0, Math.PI*2);
-            ctx.fillStyle = '#ffeeaa'; ctx.fill();
+            ctx.beginPath(); ctx.arc(0, 0, this.radius * 0.6, 0, Math.PI*2); ctx.fillStyle = '#ffeeaa'; ctx.fill();
         } else if (this.type === 'mg') {
             ctx.beginPath(); ctx.rect(-3, -1, 6, 2); 
             ctx.fillStyle = this.color; ctx.shadowBlur = 5; ctx.shadowColor = this.color; ctx.fill();
-        } else if (this.type === 'seraph_c') {
-            if (images.lightning.complete) {
-                const scale = 0.15;
-                const w = images.lightning.width * scale;
-                const h = images.lightning.height * scale;
-                ctx.shadowBlur = 15; ctx.shadowColor = '#00ffff'; 
-                ctx.drawImage(images.lightning, -w/2, -h/2, w, h);
-            }
-        } else if (this.type === 'seraph_spark' || this.type === 'seraph_x') {
-            if (images.static.complete) {
-                const scale = 0.15;
-                const w = images.static.width * scale;
-                const h = images.static.height * scale;
-                ctx.shadowBlur = 15; ctx.shadowColor = '#00ffff'; 
-                ctx.drawImage(images.static, -w/2, -h/2, w, h);
-            }
-        } else {
+        } else if (this.type === 'seraph_c' && images.lightning.complete) {
+            const scale = 0.15; const w = images.lightning.width * scale; const h = images.lightning.height * scale;
+            ctx.shadowBlur = 15; ctx.shadowColor = '#00ffff'; ctx.drawImage(images.lightning, -w/2, -h/2, w, h);
+        } else if ((this.type === 'seraph_spark' || this.type === 'seraph_x') && images.static.complete) {
+            const scale = 0.15; const w = images.static.width * scale; const h = images.static.height * scale;
+            ctx.shadowBlur = 15; ctx.shadowColor = '#00ffff'; ctx.drawImage(images.static, -w/2, -h/2, w, h);
+        } else if (this.type === 'destro_missile' && images.destroMissile.complete) {
+            const w = images.destroMissile.width * 0.15; const h = images.destroMissile.height * 0.15;
+            ctx.shadowBlur = 15; ctx.shadowColor = '#ff0000'; ctx.drawImage(images.destroMissile, -w/2, -h/2, w, h);
+        } else if ((this.type === 'destro_rocket' || this.type === 'destro_up') && images.destroRocket.complete) {
+            ctx.rotate(Math.PI/2); // Re-orients the PNG to point relative to velocity
+            const w = images.destroRocket.width * 0.15; const h = images.destroRocket.height * 0.15;
+            ctx.shadowBlur = 15; ctx.shadowColor = '#ffaa00'; ctx.drawImage(images.destroRocket, -w/2, -h/2, w, h);
+        } else if (this.type !== 'destro_rocket' && this.type !== 'destro_up') {
             ctx.beginPath(); ctx.arc(0, 0, this.radius, 0, Math.PI*2);
             ctx.fillStyle = this.color; ctx.shadowBlur = 10; ctx.shadowColor = this.color; ctx.fill();
         }
