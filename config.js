3c0bd996cc8629f438d354c3f1120941100cfff0
William1738777 <sparringbible@gmail.com>
2026-06-22 23:26:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const images = {
     destroRocket: new Image(), destroMissile: new Image(), target: new Image(), firebolt: new Image(), phantomMissile: new Image(), phantomSGMissile: new Image(), phantomp: new Image(), phantomp2: new Image(),
     abyssOrb: new Image(), abyssProj: new Image(), auraThing: new Image(), orionProj: new Image(), tempestProj: new Image(), tempestTyphoon: new Image(), tempestWindCutter: new Image(), blackoutProj: new Image(),
     
-    // NEW RAID ASSETS
+    // NPC ASSETS
     snowGrizzly: new Image(), snowPyro: new Image(), usfGrizzly: new Image(), turret: new Image(), smallTurret: new Image()
 };
 
@@ -16,14 +16,10 @@ const totalAssets = Object.keys(images).length;
 function onAssetLoad() {
     loadedCount++;
     if (loadedCount >= totalAssets) { 
-        const btnStart = document.getElementById('btn-start');
-        if (btnStart) {
-            btnStart.innerText = "Single Player"; 
-            btnStart.disabled = false;
-        }
-        const btnOnline = document.getElementById('btn-online');
-        if (btnOnline) {
-            btnOnline.style.display = 'block';
+        const btnMasterPlay = document.getElementById('btn-master-play');
+        if (btnMasterPlay) {
+            btnMasterPlay.innerText = "PLAY"; 
+            btnMasterPlay.disabled = false;
         }
     }
 }
@@ -91,19 +87,19 @@ images.smallTurret.src = 'assets/TurretSmall.png';
 
 // --- GAME CONFIG & DATA ---
 const tanksData = [
-    { id: 'grizzly', name: 'GRIZZLY', img: images.grizzly, color: '#b533ff', desc: 'Normal [C | \']: Cannon (10)<br>Skill 1 [X | ;]: Cluster (6)<br>Skill 2 [Z | L]: Heavy Missile (15)' },
-    { id: 'pyro', name: 'PYRO', img: images.pyro, color: '#ff4500', desc: 'Normal [C | \']: Ricochet (7, 4th shot Firebolt)<br>Skill 1 [X | ;]: Instant Dash (Leaves Fire Trail)<br>Skill 2 [Z | L]: Dash & Flamethrower (Passive: Shield on 3rd dash)' },
-    { id: 'scorpion', name: 'SCORPION', img: images.scorpion, color: '#00ff66', desc: 'Normal [C | \']: Toxic Burst (3x3)<br>Skill 1 [X | ;]: Hook Arrow (Pull & Jam)<br>Skill 2 [Z | L]: Poison Leak (DoT)' },
-    { id: 'dreadnaught', name: 'DREADNAUGHT', img: images.dreadnaught, color: '#8b5a2b', desc: 'Normal [C | \']: Heavy Shell (14)<br>Skill 1 [X | ;]: Dual MG (0.6/shot)<br>Skill 2 [Z | L]: Field Mine (20 Dmg/Knockback)', maxHp: 150, speedMod: 0.5, scaleMod: 1.2 },
-    { id: 'seraph', name: 'SERAPH', img: images.seraph, color: '#00ffff', desc: 'Normal [C | \']: Shock Bullet (5 Dmg, 5th shot AoE)<br>Skill 1 [X | ;]: Spark Missile (Emitter)<br>Skill 2 [Z | L]: Lightning Beam (Hold, requires Energy)', maxHp: 100 },
-    { id: 'destroyer', name: 'DESTROYER', img: images.destroyer, color: '#888888', desc: 'Normal [C | \']: Cannon (10)<br>Skill 1 [X | ;]: Mortar Strike (Hold to Lock)<br>Skill 2 [Z | L]: Ghost Missile (Pierces Walls, Wall-Slam Stun)', speedMod: 0.85, maxHp: 120 },
-    { id: 'phantom', name: 'PHANTOM', img: images.phantomA, color: '#9d00ff', desc: 'Normal [C | \']: Bouncing Missiles (60% Hit Refund)<br>Skill 1 [X | ;]: Plasma Shotgun<br>Skill 2 [Z | L]: Evasive Maneuvers' },
-    { id: 'abyss', name: 'ABYSS', img: images.abyss, color: '#1a0033', desc: 'Normal [C | \']: Shadow Bolt (Hold for Rapid Fire)<br>Skill 1 [X | ;]: Void Orb (Deploy trap, shoot 15x for Domain)<br>Skill 2 [Z | L]: Event Horizon (Bouncing Black Hole)' },
-    { id: 'orion', name: 'ORION', img: images.orion, color: '#ff33cc', desc: 'Normal [C | \']: Shockwave Missile (Bounces/Portals)<br>Skill 1 [X | ;]: Chronosphere (Slow & Reflect)<br>Skill 2 [Z | L]: Quantum Portals (Tap) / Anti-Grav Lift (Hold)' },
-    { id: 'tempest', name: 'TEMPEST', img: images.tempest, color: '#aaffff', desc: 'Normal [C | \']: Wind Shot Dash (Alternates Shoot/Dash)<br>Skill 1 [X | ;]: Typhoon\'s Embrace (Consumes 3 Marks, Shields, Trap)<br>Skill 2 [Z | L]: Windcutter (Slow Converging Crescents)', maxHp: 60, speedMod: 1.45 },
-    { id: 'blackout', name: 'BLACKOUT', img: images.blackout, color: '#33ff33', desc: 'Normal [C | \']: Snipe (High speed, random dmg, disrupts aim)<br>Skill 1 [X | ;]: Teleport Anchor (Teleport resets C. Hold to clear)<br>Skill 2 [Z | L]: Mark Trap (Hold to aim, triggers on step)', maxHp: 60 },
+    { id: 'grizzly', name: 'GRIZZLY', img: images.grizzly, color: '#b533ff', desc: 'Normal: Cannon (10)<br>Skill 1 [X]: Cluster (6)<br>Skill 2 [Z]: Heavy Missile (15)' },
+    { id: 'pyro', name: 'PYRO', img: images.pyro, color: '#ff4500', desc: 'Normal: Ricochet (7, 4th shot Firebolt)<br>Skill 1 [X]: Instant Dash (Leaves Fire Trail)<br>Skill 2 [Z]: Dash & Flamethrower (Passive: Shield on 3rd dash)' },
+    { id: 'scorpion', name: 'SCORPION', img: images.scorpion, color: '#00ff66', desc: 'Normal: Toxic Burst (3x3)<br>Skill 1 [X]: Hook Arrow (Pull & Jam)<br>Skill 2 [Z]: Poison Leak (DoT)' },
+    { id: 'dreadnaught', name: 'DREADNAUGHT', img: images.dreadnaught, color: '#8b5a2b', desc: 'Normal: Heavy Shell (14)<br>Skill 1 [X]: Dual MG (0.6/shot)<br>Skill 2 [Z]: Field Mine (20 Dmg/Knockback)', maxHp: 150, speedMod: 0.5, scaleMod: 1.2 },
+    { id: 'seraph', name: 'SERAPH', img: images.seraph, color: '#00ffff', desc: 'Normal: Shock Bullet (5 Dmg, 5th shot AoE)<br>Skill 1 [X]: Spark Missile (Emitter)<br>Skill 2 [Z]: Lightning Beam (Hold, requires Energy)', maxHp: 100 },
+    { id: 'destroyer', name: 'DESTROYER', img: images.destroyer, color: '#888888', desc: 'Normal: Cannon (10)<br>Skill 1 [X]: Mortar Strike (Hold to Lock)<br>Skill 2 [Z]: Ghost Missile (Pierces Walls, Wall-Slam Stun)', speedMod: 0.85, maxHp: 120 },
+    { id: 'phantom', name: 'PHANTOM', img: images.phantomA, color: '#9d00ff', desc: 'Normal: Bouncing Missiles (60% Hit Refund)<br>Skill 1 [X]: Plasma Shotgun<br>Skill 2 [Z]: Evasive Maneuvers' },
+    { id: 'abyss', name: 'ABYSS', img: images.abyss, color: '#1a0033', desc: 'Normal: Shadow Bolt (Hold for Rapid Fire)<br>Skill 1 [X]: Void Orb (Deploy trap, shoot 15x for Domain)<br>Skill 2 [Z]: Event Horizon (Bouncing Black Hole)' },
+    { id: 'orion', name: 'ORION', img: images.orion, color: '#ff33cc', desc: 'Normal: Shockwave Missile (Bounces/Portals)<br>Skill 1 [X]: Chronosphere (Slow & Reflect)<br>Skill 2 [Z]: Quantum Portals (Tap) / Anti-Grav Lift (Hold)' },
+    { id: 'tempest', name: 'TEMPEST', img: images.tempest, color: '#aaffff', desc: 'Normal: Wind Shot Dash (Alternates Shoot/Dash)<br>Skill 1 [X]: Typhoon\'s Embrace (Consumes 3 Marks, Shields, Trap)<br>Skill 2 [Z]: Windcutter (Slow Converging Crescents)', maxHp: 60, speedMod: 1.45 },
+    { id: 'blackout', name: 'BLACKOUT', img: images.blackout, color: '#33ff33', desc: 'Normal: Snipe (High speed, random dmg, disrupts aim)<br>Skill 1 [X]: Teleport Anchor (Teleport resets C. Hold to clear)<br>Skill 2 [Z]: Mark Trap (Hold to aim, triggers on step)', maxHp: 60 },
     
-    // --- RAID MODE UNITS ---
+    // --- NPC UNITS ---
     { id: 'usf_grizzly', name: 'USF GRIZZLY', img: images.usfGrizzly, color: '#33aa33', desc: 'Allied Frontline', maxHp: 100, npcOnly: true },
     { id: 'snow_grizzly', name: 'SNOW GRIZZLY', img: images.snowGrizzly, color: '#aaddff', desc: 'Enemy Support (No Heavy Missile)', maxHp: 100, aiNoZ: true, npcOnly: true },
     { id: 'snow_pyro', name: 'SNOW PYRO', img: images.snowPyro, color: '#ff4444', desc: 'Enemy Rusher', maxHp: 100, npcOnly: true },
@@ -137,25 +133,37 @@ const mapsData = [
         rocks: [ {x: 250, y: 200, r: 40}, {x: 750, y: 200, r: 40}, {x: 250, y: 500, r: 40}, {x: 750, y: 500, r: 40}, {x: 500, y: 350, r: 60} ]
     },
     {
-        id: 'raid_facility', name: 'RAID: ENEMY FACILITY', bgImg: 'RaidModeBG', width: 3000, height: 2000,
+        id: 'team_skirmish', name: 'TEAM SKIRMISH (3V3)', bgImg: 'RaidModeBG', width: 3000, height: 2000,
         walls: [ 
-            // Top and Bottom enclosing walls for the facility on the right
-            {x: 2300, y: 400, w: 700, h: 40},
-            {x: 2300, y: 1560, w: 700, h: 40},
-            // Left-side facing wall of the facility with 2 entrance gaps
-            {x: 2300, y: 440, w: 40, h: 360},  // Top segment
-            {x: 2300, y: 1000, w: 40, h: 200}, // Middle segment between doors
-            {x: 2300, y: 1400, w: 40, h: 160}, // Bottom segment
-            // Inner defensive barricades
-            {x: 2600, y: 750, w: 150, h: 40},
-            {x: 2600, y: 1250, w: 150, h: 40}
+            // TOP LANE WALLS
+            {x: 800, y: 350, w: 400, h: 50},
+            {x: 1800, y: 350, w: 400, h: 50},
+            
+            // BOTTOM LANE WALLS
+            {x: 800, y: 1600, w: 400, h: 50},
+            {x: 1800, y: 1600, w: 400, h: 50},
+
+            // MIDDLE LANE VERTICAL FLANK COVER
+            {x: 900, y: 750, w: 50, h: 500},
+            {x: 2050, y: 750, w: 50, h: 500}
         ],
         rocks: [ 
-            // Cover scattered approaching the facility
-            {x: 1800, y: 800, r: 80}, {x: 1800, y: 1200, r: 80},
-            {x: 1400, y: 1000, r: 120}, {x: 2100, y: 500, r: 60}, {x: 2100, y: 1500, r: 60}
+            // TEAM 1 SPAWN COVER
+            {x: 400, y: 1000, r: 120},
+            {x: 600, y: 600, r: 70},
+            {x: 600, y: 1400, r: 70},
+
+            // TEAM 2 SPAWN COVER
+            {x: 2600, y: 1000, r: 120},
+            {x: 2400, y: 600, r: 70},
+            {x: 2400, y: 1400, r: 70},
+
+            // CENTRAL ARENA CHAOS (The Pit)
+            {x: 1500, y: 1000, r: 150}, // Giant center rock
+            {x: 1300, y: 700, r: 80},
+            {x: 1700, y: 700, r: 80},
+            {x: 1300, y: 1300, r: 80},
+            {x: 1700, y: 1300, r: 80}
         ]
     }
 ];
-
-const spawnPoints = [ {x: 100, y: 350}, {x: 900, y: 350}, {x: 150, y: 150}, {x: 850, y: 550} ];
